6d199d61e14d7fd58ae86db15a9dcafb4e50004d
amrwaheed <amr.waheed00@outlook.com>
2021-12-22 22:26:55 +0200

    firebase deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,16 @@ export const environment = {
   production: false,
   app_id: 'beb304c3',
   app_key: 'a03848fda0086729c9da6361b53d8d96',
-  api_url_base: "https://api.edamam.com/api/"
+  api_url_base: "https://api.edamam.com/api/",
+  firebaseConfig : {
+    apiKey: "AIzaSyATR986YL0WC_TStnQhdjcR8MtsGCgMETA",
+    authDomain: "nutrition-app-7.firebaseapp.com",
+    projectId: "nutrition-app-7",
+    storageBucket: "nutrition-app-7.appspot.com",
+    messagingSenderId: "231219397747",
+    appId: "1:231219397747:web:05fd509b5172aceb9ed606",
+    measurementId: "G-W6Z89D44LY"
+  }
 };
 
 /*
